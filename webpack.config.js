a0e459a21dc514acb1417515b817d1032bbad610
seol <>
2020-04-22 00:16:43 +0900

    add source

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const banner = require("./banner.js");
 
 module.exports = {
-  mode: "development",
+  mode: "development", // development, production
   entry: {
-    main: "./src/app.js",
+    main: "./app.js",
   },
   output: {
     filename: "[name].js",
@@ -15,7 +18,12 @@ module.exports = {
     rules: [
       {
         test: /\.css$/, // .css 확장자로 끝나는 모든 파일
-        use: ["style-loader", "css-loader"], // css-loader를 적용한다
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader // 프로덕션 환경
+            : "style-loader", // 개발 환경
+          "css-loader",
+        ], // css-loader를 적용한다
       },
       {
         test: /\.png$/,
@@ -28,7 +36,36 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader", // 바벨 로더를 추가한다
+      },
     ],
   },
-  plugins: [new webpack.BannerPlugin(banner)],
+  plugins: [
+    new webpack.BannerPlugin(banner),
+    new webpack.DefinePlugin({
+      TEST: "1+1",
+      TEST1: JSON.stringify("TT"),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // 템플릿 경로를 지정
+      templateParameters: {
+        // 템플릿에 주입할 파라매터 변수 지정
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true, // 주석 제거
+            }
+          : false,
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : []),
+  ],
 };
